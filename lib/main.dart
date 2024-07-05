d7e9011e8a88a76edc8361e9a49b9ae81372c446
penguin <penguin.qlc@gmail.com>
2024-07-05 14:29:38 +0100

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'dart:async';
+import 'package:wakelock/wakelock.dart';
 
 void main() {
   runApp(MeditationApp());
@@ -27,14 +28,19 @@ class _HomePageState extends State<HomePage> {
   String _phase = "Inhale";
   int _seconds = 4;
   bool _isRunning = false;
+  String _currentTimer = 'none'; // Variable to track the current running timer
 
-  void _startTimer(int inhaleSeconds, int holdSeconds, int exhaleSeconds) {
+  void _startTimer(int inhaleSeconds, int holdSeconds, int exhaleSeconds, String timerType) {
     setState(() {
       _isRunning = true;
       _progress = 0.0;
       _phase = "Inhale";
       _seconds = inhaleSeconds;
+      _currentTimer = timerType; // Set the current timer type
     });
+
+    // Enable wakelock to keep the screen on
+    Wakelock.enable();
 
     _timer = Timer.periodic(Duration(milliseconds: 50), (timer) {
       setState(() {
@@ -61,53 +67,66 @@ class _HomePageState extends State<HomePage> {
       _isRunning = false;
       _progress = 0.0;
       _timer?.cancel();
+      _currentTimer = 'none'; // Reset the current timer type
     });
+
+    // Disable wakelock to allow the screen to turn off
+    Wakelock.disable();
   }
 
   @override
   void dispose() {
     _timer?.cancel();
+    Wakelock.disable(); // Ensure wakelock is disabled when the widget is disposed
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Brease')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircularPercentIndicator(
-              radius: 100.0,
-              lineWidth: 10.0,
-              percent: _progress,
-              center: Text(
-                _phase,
-                style: TextStyle(fontSize: 20.0),
+      //appBar: AppBar(title: Text('Brease')),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.jpeg'), // Path to your background image
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularPercentIndicator(
+                radius: 100.0,
+                lineWidth: 10.0,
+                percent: _progress,
+                center: Text(
+                  _phase,
+                  style: TextStyle(fontSize: 40.0, color: Colors.white70), // Changed color to white
+                ),
+                progressColor: Colors.blue,
               ),
-              progressColor: Colors.blue,
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: _isRunning ? _stopTimer : () {
-                    _startTimer(4, 4, 6); // Short button: inhale 4s, hold 2s, exhale 6s
-                  },
-                  child: Text(_isRunning ? 'Stop' : 'Short'),
-                ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                  onPressed: _isRunning ? _stopTimer : () {
-                    _startTimer(4, 6, 8); // Long button: inhale 4s, hold 6s, exhale 8s
-                  },
-                  child: Text('Long'),
-                ),
-              ],
-            ),
-          ],
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: _isRunning && _currentTimer == 'short' ? _stopTimer : () {
+                      _startTimer(4, 4, 6, 'short'); // Short button: inhale 4s, hold 4s, exhale 6s
+                    },
+                    child: Text(_isRunning && _currentTimer == 'short' ? 'Stop' : 'Short'),
+                  ),
+                  SizedBox(width: 20),
+                  ElevatedButton(
+                    onPressed: _isRunning && _currentTimer == 'long' ? _stopTimer : () {
+                      _startTimer(4, 6, 8, 'long'); // Long button: inhale 4s, hold 6s, exhale 8s
+                    },
+                    child: Text(_isRunning && _currentTimer == 'long' ? 'Stop' : 'Long'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
